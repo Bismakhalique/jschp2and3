79abc76223dfa472498bf477a67737b0ae73759d
Bismakhalique <121749134+Bismakhalique@users.noreply.github.com>
2023-03-12 01:04:42 -0800

    Add files via upload

---
+++ chp 2and 3 js/variables.js
@@ -0,0 +1,82 @@
+var username="BISMA";
+ var myname="BISMA KHALIQUE"
+
+/////////////////////////////////////////////////////////////////////////////
+var message;
+var message="hello world"
+alert(message);
+
+var name="BISMA"
+var age="18 year old"
+var admited="enroll in web development and mobile applicaton course"
+alert(name);
+alert( age);
+alert(admited);
+
+
+
+/////////////////////////////////////////////////////////////////////////
+var PIZZ="pizza \n Pizz \n Piz \n Pi \n P"
+alert(PIZZ);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+var x =" my email address is "
+ var y ="email@example.com"
+ var z =x+y;
+alert(z);
+
+
+/////////////////////////////////////////////////////////////////////////////
+var xyz =" i m trying to learn from the book A smarter way to learn JavaScript"
+alert(xyz);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+//var myname ="yah! i can write html content through js"
+ 
+ //document.write="myname"
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+var efg ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(efg);
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+var age1 ="                                             i m 19 years old                                         "
+alert(age1);
+
+
+
+/////////////////////////////////////////////////////////////////
+
+
+
+var visitor=14;
+alert("you have visited this site "+16+"times")
+
+
+///////////////////////////////////////////////////////////////
+
+// var birthyear=2003
+//document.write("you have" "+birthyear+"" years old");
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+var name="BISMA";
+var quantity=6;
+var product="t-shirt";
+document.write(name+" ordered "+quantity+" " +product+ "  from LEVIS clothing store");
